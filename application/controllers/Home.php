f12d7d6ac277ffbc4b989bb2569214c96d33c381
iamabhyas <abhyas.deshpremi@techment.com>
2022-01-08 09:49:56 +0530

    Update Home.php

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,6 +3,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
+	function __construct() {
+        parent::__construct();
+        if($this->session->userdata('isUserLoggedIn') != TRUE){ 
+            redirect('/login');
+        }
+    }
+
 	public function index()
 	{
 		$data = array();
