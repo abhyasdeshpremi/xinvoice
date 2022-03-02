85a3e71ad624e011e9825631b7e5c86d45489387
iamabhyas <abhyas.deshpremi@techment.com>
2022-03-02 19:24:47 +0530

    signup

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -37,6 +37,84 @@ class Login_model extends CI_Model {
         }
         return $data;
     }
+
+    public function firmUniqueCode($firm_Code){
+        $data = array();
+        $this->db->where('firm_code', trim($firm_Code));
+        $query = $this->db->get('Firms');
+        if($query->num_rows() === 1){
+            $data['flag'] = TRUE;
+        }else{
+            $data['flag'] = FALSE;
+        }
+        return $data;
+    }
+
+    public function checkuseremail($email){
+        $data = array();
+        $this->db->where('email', trim($email));
+        $query = $this->db->get('Users');
+        if($query->num_rows() === 1){
+            $data['flag'] = TRUE;
+        }else{
+            $data['flag'] = FALSE;
+        }
+        return $data;
+    }
+
+    public function checkusername($username){
+        $data = array();
+        $this->db->where('username', trim($username));
+        $query = $this->db->get('Users');
+        if($query->num_rows() === 1){
+            $data['flag'] = TRUE;
+        }else{
+            $data['flag'] = FALSE;
+        }
+        return $data;
+    }
+
+    public function registeruser($data){
+        $datalist = array();
+
+        $dataFirm = array(
+            'firm_code'=>strtoupper($data['firmuniquecode']),
+            'name'=>$data['firmName'],
+            'firm_email'=>$data['firmEmail'],
+            'address'=>$data['firmAddress'],
+            'area'=>$data['firmArea'],
+            'city'=>$data['firmCity'],
+            'district'=>$data['firmdistrict'],
+            'state'=>$data['firmState'],
+            'pin_code'=>$data['firmZip'],
+            'mobile_number'=>$data['firmMobile'],
+            'fk_username'=> 'REGISTERUSER'
+        );
+        $this->db->insert('Firms',$dataFirm);
+        $datalist['firminsertFlag'] = ($this->db->affected_rows() != 1) ? false : true;
+        $datalist['firmlastquery'] = $this->db->last_query();
+
+        $dataUser = array(
+            'username'=>strtoupper($data['username']),
+            'email'=>$data['firmEmail'],
+            'password'=>md5(trim($data['password'])),
+            'first_name'=>$data['firstname'],
+            'last_name'=>$data['lastname'],
+            'mobile_number'=>$data['firmMobile'],
+            'status'=>'active',
+            'role'=>'admin',
+            'fk_firm_code'=> strtoupper($data['firmuniquecode']),
+            'fk_username'=> 'REGISTERUSER'
+        );
+        $this->db->insert('Users',$dataUser);
+        $datalist['userinsertFlag'] = ($this->db->affected_rows() != 1) ? false : true;
+        $datalist['userlastquery'] = $this->db->last_query();
+
+        
+
+        return $datalist;
+    }
+    
 }
 
 ?>
--- application/views/layouts/login_layout.php
+++ application/views/layouts/login_layout.php
@@ -11,6 +11,9 @@
         <link rel="icon" type="image/x-icon" href="<?php echo base_url();?>assets/img/favicon.png" />
         <script data-search-pseudo-elements defer src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.28.0/feather.min.js" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="<?php echo base_url();?>assets/js/scripts.js"></script>
     </head>
     <body class="bg-primary">
         <div id="layoutAuthentication">
@@ -36,8 +39,5 @@
                 </footer>
             </div>
         </div>
-        <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="<?php echo base_url();?>assets/js/scripts.js"></script>
     </body>
 </html>
