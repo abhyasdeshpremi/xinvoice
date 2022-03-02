fc2baf352e57bf096f29b24a8163a33a2015df56
iamabhyas <abhyas.deshpremi@techment.com>
2022-03-02 19:32:31 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'https://irotore.com';
-$config['base_url'] = 'http://localhost/xinvoice/';
+$config['base_url'] = 'https://irotore.com';
+// $config['base_url'] = 'http://localhost/xinvoice/';
 
 /*
 |--------------------------------------------------------------------------
