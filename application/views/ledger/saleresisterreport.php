4d509c6882972a47bdf398f825ca6a015f20a742
iamabhyas <abhyas.deshpremi@techment.com>
2022-03-13 20:49:59 +0530

    Update saleresisterreport.php

--- application/views/ledger/saleresisterreport.php
+++ application/views/ledger/saleresisterreport.php
@@ -1,9 +1,7 @@
 <div class="datatable">
     <div id="dataTable_wrapper" class="dataTables_wrapper dt-bootstrap4 pt-3">
         <div class="row">
-            <div class="col-md-2">
-            </div>
-            <div class="col-md-2">
+            <div class="col-md-4">
                 <input class="form-control border-end-0 border rounded-pill" type="text" placeholder="search" id="salesearch">
             </div>
             <div class="clearfix"></div>
