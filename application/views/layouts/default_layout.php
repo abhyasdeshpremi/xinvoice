7b12436fd9fae86a82c0c156bf1f794889dfafd2
iamabhyas <abhyas.deshpremi@techment.com>
2022-03-17 14:51:33 +0530

    sale report graph

--- application/views/layouts/default_layout.php
+++ application/views/layouts/default_layout.php
@@ -522,10 +522,11 @@
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="<?php echo base_url();?>assets/js/scripts.js"></script>
-        <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js" crossorigin="anonymous"></script>
         <script src="<?php echo base_url();?>assets/demo/chart-area-demo.js"></script>
         <script src="<?php echo base_url();?>assets/demo/chart-bar-demo.js"></script>
-        <script src="<?php echo base_url();?>assets/demo/chart-pie-demo.js"></script> -->
+        <script src="<?php echo base_url();?>assets/demo/chart-pie-demo.js"></script>
+        <script src="<?php echo base_url();?>assets/demo/chart-sell-bar.js"></script>
         <script src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js" crossorigin="anonymous"></script>
         <script type="text/javascript" src="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.js" crossorigin="anonymous"></script>
         <script src="<?php echo base_url('assets/demo/date-range-picker-demo.js'); ?>"></script>
