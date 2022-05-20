ebf3b48b13592e7fbf8fd2762f0ee5656d82d6c9
iamabhyas <abhyas.deshpremi@techment.com>
2022-05-20 10:57:50 +0530

    Update Order_model.php

--- application/models/Order_model.php
+++ application/models/Order_model.php
@@ -22,9 +22,9 @@ class Order_model extends CI_Model {
 
     public function order_list($limit, $start, $order_type = 'sell'){
         $this->db->limit($limit, $start);
-        if (($this->session->userdata('role') != "superadmin") || ($this->session->userdata('role') != "admin") ){
-            $this->db->where('fk_username', $this->session->userdata('username'));
-        }
+        // if (($this->session->userdata('role') != "superadmin") || ($this->session->userdata('role') != "admin") ){
+        //     $this->db->where('fk_username', $this->session->userdata('username'));
+        // }
         $this->db->where('fk_firm_code', $this->session->userdata('firmcode'));
         $this->db->where('order_type', $order_type);
         $this->db->order_by("created_at", "DESC");
