6c78bf470becb12335994ace6f9995fb0beb82c6
iamabhyas <sourabh@floodlightdigital.com>
2022-07-31 21:01:59 +0530

    correction

--- application/views/invoice/createinvoicestandardpdf.php
+++ application/views/invoice/createinvoicestandardpdf.php
@@ -55,13 +55,16 @@ if($paper_size == 'A5'){
 ?>
 <body style="<?php echo $width; ?>" >
     <center><div style="<?php echo $width; ?>" ><span style="font:12px;"><?php echo $invoiceTitle; ?></span></div></center>
+    <div>
     <span style="font:10px; float:left;">GSTIN : <b><?php echo isset($owninvoicegstin) ? $owninvoicegstin : ''; ?></b></span>
     <span style="font:10px; float:right; margin-right:10px;"><i>Original Copy</i></span>
-    
+    </div>
     <center>
+        <div style="<?php echo $width; ?>" >
         <span style="font:16px; font-style:bold;"><?php echo isset($invoicetitle) ? $invoicetitle : '';?></span><br>
         <span style="font:10px;"><?php echo $invoicesubtitle;?></span><br>
         <span style="font:10px;"><?php echo ($this->session->userdata('pan_number')) ? "PAN : ".$this->session->userdata('pan_number') : ''; ?></span>
+        </div>
     </center>
     
     <center style="margin-right:0px;">
